6653126677af046947c33ef66160ac5eab5524dd
Vetrik <vetrik77@gmail.com>
2020-08-14 10:28:01 -0400

    Add project files.

---
+++ ApiControllers/Controllers/ReservationController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApiControllers.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace ApiControllers.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ReservationController : ControllerBase
+    {
+        private IRepository repository;
+        public ReservationController(IRepository repo) => repository = repo;
+
+        [HttpGet]
+        public IEnumerable<Reservation> Get() => repository.Reservations;
+
+        [HttpGet("{id}")]
+        public Reservation Get(int id) => repository[id];
+
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res) =>
+            repository.AddReservation(new Reservation
+            {
+                Name = res.Name,
+                StartLocation = res.StartLocation,
+                EndLocation = res.EndLocation
+            });
+
+        [HttpPut]
+        public Reservation Put([FromBody] Reservation res) => repository.UpdateReservation(res);
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody]JsonPatchDocument<Reservation> patch)
+        {
+            Reservation res = Get(id);
+            if (res != null)
+            {
+                patch.ApplyTo(res);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => repository.DeleteReservation(id);
+    }
+}
